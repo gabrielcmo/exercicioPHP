3b517519a2a94a17defabdb0276d997768508dab
gabriel <gabrieloliveira9669@gmail.com>
2018-10-02 15:38:25 -0300

    primeiro commit

---
+++ init.php
@@ -0,0 +1,12 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'estoque');
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+date_default_timezone_set('America/Sao_Paulo');
+
+require_once 'functions.php';
